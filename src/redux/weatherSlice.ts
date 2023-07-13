40eaf4278719f582633cc2aaf14dcf03d55eb536
andy <hsbsbbdn@gmail.com>
2023-07-13 16:32:01 +0800

    feat: add weatherSlice

---
+++ src/redux/weatherSlice.ts
@@ -0,0 +1,61 @@
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { CurrentType, LocationType } from '@/Interface/I_weather';
+
+interface IWeatherState {
+  location: LocationType;
+  current: CurrentType;
+}
+
+const initialState: IWeatherState = {
+  location: {
+    name: '',
+    region: '',
+    country: '',
+    lat: 0,
+    lon: 0,
+    tz_id: '',
+    localtime_epoch: 0,
+    localtime: 0,
+  },
+  current: {
+    last_updated_epoch: 0,
+    last_updated: '',
+    temp_c: 0,
+    temp_f: 0,
+    is_day: 0,
+    condition: {
+      text: '',
+      icon: '',
+      code: 0,
+    },
+    wind_mph: 0,
+    wind_kph: 0,
+    wind_degree: 0,
+    wind_dir: '',
+    pressure_mb: 0,
+    pressure_in: 0,
+    precip_mm: 0,
+    precip_in: 0,
+    humidity: 0,
+    cloud: 0,
+    feelslike_c: 0,
+    feelslike_f: 0,
+    vis_km: 0,
+    vis_miles: 0,
+    uv: 0,
+    gust_mph: 0,
+    gust_kph: 0,
+  },
+};
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {
+    getWeather: () => undefined,
+  },
+});
+
+export const { getWeather } = weatherSlice.actions;
+export default weatherSlice.reducer;
